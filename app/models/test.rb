5522157b3e1f1c2a07fb43fb3194ee4d8d02db8e
SatanSoo12 <zarmeen651@gmail.com>
2020-09-08 16:30:35 +0500

    zarmeen: changing association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
-	has_many :bill_details as: :billable
-	has_many :lab_reports
-	has_many :bills, through: :bill_details
+	has_many :bill_details as: :billable, dependent: :destroy
+	has_many :lab_reports, dependent: :destroy
+	has_many :bills, through: :bill_details, dependent: :destroy
 
 end
