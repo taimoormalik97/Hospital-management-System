af11ceda38639dcfb54b4037d762dbb307fcc362
Zarmeen <zarmeen651@gmail.com>
2020-09-08 16:46:44 +0500

    Update 20200908065232_create_medicines.rb

--- db/migrate/20200908065232_create_medicines.rb
+++ db/migrate/20200908065232_create_medicines.rb
@@ -1,9 +1,9 @@
 class CreateMedicines < ActiveRecord::Migration[6.0]
   def change
     create_table :medicines do |t|
-      t.string :name
-      t.integer :quantity
-      t.decimal :price, precision: 10, scale: 2
+      t.string :name,  null: false
+      t.integer :quantity,  null: false
+      t.decimal :price, precision: 10, scale: 2,  null: false
 
       t.timestamps
     end
