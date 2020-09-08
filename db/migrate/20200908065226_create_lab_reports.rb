0dd1f5e6a53d9a6f33b583b0d2b5d75920d0e8f9
Zarmeen <zarmeen651@gmail.com>
2020-09-08 16:41:13 +0500

    Update 20200908065226_create_lab_reports.rb

--- db/migrate/20200908065226_create_lab_reports.rb
+++ db/migrate/20200908065226_create_lab_reports.rb
@@ -3,8 +3,8 @@ class CreateLabReports < ActiveRecord::Migration[6.0]
     create_table :lab_reports do |t|
       t.references :patient, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.datetime :sample_collection_date
-      t.datetime :report_generation_date
+      t.datetime :sample_collection_date,  null: false
+      t.datetime :report_generation_date, null: false
 
       t.timestamps
     end
