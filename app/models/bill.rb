1e2fd647a7ebb5ed80ae0d96e67f37c426d68ab4
Haider <haidermalik6789@gmail.com>
2020-09-08 16:26:34 +0500

    association changes

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,6 @@
 class Bill < ApplicationRecord
   belongs_to :patient
-  has_many :bill_details as: :billable
+  has_many :bill_details as: :billable, dependent: :destroy
   has_many :medicines through: :bill_details
   has_many :tests through: :bill_details
   has_many :doctors through: :bill_details
