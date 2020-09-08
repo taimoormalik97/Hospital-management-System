fbadc307f4b952fd330d87b65736c1a3b207dc68
taimoormalik97 <taimoor.malik@7vals.com>
2020-09-08 18:04:30 +0500

    Doctor model updated

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,6 +4,6 @@ class Doctor < User
   has_many :feedbacks, dependent: :destroy
   has_many :availabilities, dependent: :destroy
   has_many :bill_details as: :billable, dependent: :nullify
-  has_many :bills through: :bill_nullify
+  has_many :bills through: :bill_details
 
 end
