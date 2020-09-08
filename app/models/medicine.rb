4e0f5b3830c752f5c0bdb72425463f83c758d4b1
SatanSoo12 <zarmeen651@gmail.com>
2020-09-08 17:14:58 +0500

    zarmeen: updating migrations

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,7 +1,7 @@
 class Medicine < ApplicationRecord
-	has_many :bill_details as: :billable, dependent: :destroy
-	has_many :prescribed_medicines, dependent: :destroy
-	has_many :purchase_details, dependent: :destroy
+	has_many :bill_details as: :billable, dependent: :nullify
+	has_many :prescribed_medicines, dependent: :nullify
+	has_many :purchase_details, dependent: :nullify
     belongs_to :hospital
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-	has_many :bill_details as: :billable, dependent: :destroy
-	has_many :lab_reports, dependent: :destroy
-	has_many :bills, through: :bill_details, dependent: :destroy
+	has_many :bill_details as: :billable, dependent: :nullify
+	has_many :lab_reports, dependent: :nullify
+	has_many :bills, through: :bill_details
     belongs_to :hospital
 
 end
