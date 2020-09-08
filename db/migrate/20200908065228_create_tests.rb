f6f70cf6a055628aa3dc84d300e25271dfc23040
Zarmeen <zarmeen651@gmail.com>
2020-09-08 16:45:39 +0500

    Update 20200908065228_create_tests.rb

--- db/migrate/20200908065228_create_tests.rb
+++ db/migrate/20200908065228_create_tests.rb
@@ -1,9 +1,9 @@
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
-      t.string :name
-      t.string :details
-      t.decimal :price, precision: 10, scale: 2
+      t.string :name,  null: false
+      t.string :details,  null: false
+      t.decimal :price, precision: 10, scale: 2,  null: false
 
       t.timestamps
     end
