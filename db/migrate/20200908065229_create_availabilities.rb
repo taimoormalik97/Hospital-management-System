9812487d5ee2f5ac5166dd6f6a0b642a36a65990
Zarmeen <zarmeen651@gmail.com>
2020-09-08 16:46:13 +0500

    Update 20200908065229_create_availabilities.rb

--- db/migrate/20200908065229_create_availabilities.rb
+++ db/migrate/20200908065229_create_availabilities.rb
@@ -1,9 +1,9 @@
 class CreateAvailabilities < ActiveRecord::Migration[6.0]
   def change
     create_table :availabilities do |t|
-      t.string :week_day
-      t.datetime :start_slot
-      t.datetime :end_slot
+      t.string :week_day,  null: false
+      t.datetime :start_slot,  null: false
+      t.datetime :end_slot,  null: false
       t.references :doctor, null: false, foreign_key: true
 
       t.timestamps
