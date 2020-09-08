16f9e60d452ca3d12915edc4947f5b08072e84ac
taimoormalik97 <taimoor.malik@7vals.com>
2020-09-08 18:33:35 +0500

    dependent added in hospital's associations

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,14 +1,14 @@
 class Hospital < ApplicationRecord
-  has_many :users
-  has_many :appointments
-  has_many :feedbacks
-  has_many :availability
-  has_many :lab_reports
-  has_many :tests
-  has_many :medicines
-  has_many :prescribed_medicines
-  has_many :purchase_orders
-  has_many :purchase_details
-  has_many :bills
-  has_many :bill_details
+  has_many :users, dependent: :destroy
+  has_many :appointments, dependent: :destroy
+  has_many :feedbacks, dependent: :destroy
+  has_many :availability, dependent: :destroy
+  has_many :lab_reports, dependent: :destroy
+  has_many :tests, dependent: :destroy
+  has_many :medicines, dependent: :destroy
+  has_many :prescribed_medicines, dependent: :destroy
+  has_many :purchase_orders, dependent: :destroy
+  has_many :purchase_details, dependent: :destroy
+  has_many :bills, dependent: :destroy
+  has_many :bill_details, dependent: :destroy
 end
