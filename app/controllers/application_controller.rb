fbc7e51629ece3147b7100ebd37eea4124ab70bb
Zarmeen <zarmeen651@gmail.com>
2020-09-14 16:28:52 +0500

    Update app/controllers/application_controller.rb
    Co-authored-by: MirahSaqib <52164165+MirahSaqib@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound do
+    respond_to do |format|
+      format.html { render plain: '404 Not Found', status: 404 }
+    end
+  end
 
   private
 
