eebc900cb01935d31774acdda4d200eca5c49e17
taimoormalik97 <taimoor.malik@7vals.com>
2020-09-15 17:01:32 +0500

    respond_to block added

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -2,21 +2,28 @@ class PublicPagesController < ApplicationController
 
   # GET /resource/index
   def index
-
+    respond_to do |format|
+      format.html
+    end
   end
 
   # GET /resource/find
   def find
     @user = User.new
+    respond_to do |format|
+      format.html
+    end
   end
 
   # POST/resource/check_email
   def check_email
-    user = user_email_in_params
-    if User.unscoped.where(email: user[:email]).count(:hospital_id) == 1
-      redirect_to new_user_session_url(email: user[:email], subdomain: User.unscoped.find_by_email(user[:email]).hospital.sub_domain)
-    else
-      redirect_to select_domain_url(email: user[:email])
+    respond_to do |format|
+      user = user_email_in_params
+      if User.unscoped.where(email: user[:email]).count(:hospital_id) == 1
+        format.html{redirect_to new_user_session_url(email: user[:email], subdomain: User.unscoped.find_by_email(user[:email]).hospital.sub_domain)}
+      else
+        format.html {redirect_to select_domain_url(email: user[:email])}
+      end
     end
   end
 
@@ -25,6 +32,5 @@ class PublicPagesController < ApplicationController
   def user_email_in_params
     params.require(:user).permit(:email)
   end
-
 
 end
