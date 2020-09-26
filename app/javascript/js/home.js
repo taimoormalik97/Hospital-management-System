3558c896c26b472096b2241bce3fc8470af200f3
SatanSoo12 <zarmeen651@gmail.com>
2020-09-26 15:53:42 +0500

    home page

--- app/javascript/js/home.js
+++ app/javascript/js/home.js
@@ -2,4 +2,7 @@ $(document).scroll(function () {
   var $nav = $(".nav-transparent");
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   $(".main-sign-up-btn").toggleClass('scrolled-sign-up-btn-color', $(this).scrollTop() > $nav.height());
+  $(".main-sign-in-btn").toggleClass('scrolled-sign-in-btn-color', $(this).scrollTop() > $nav.height());
+  $(".hms-nav-item").toggleClass('scrolled-hms-nav-item', $(this).scrollTop() > $nav.height());
+  $(".hms-title").toggleClass('scrolled-hms-title', $(this).scrollTop() > $nav.height());
 });
