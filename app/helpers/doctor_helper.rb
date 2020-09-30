6883c69ff4f6cbf900a79dd2cd05644e904dd854
SatanSoo12 <zarmeen651@gmail.com>
2020-09-30 16:41:53 +0500

    resolve syntax error

--- app/helpers/doctor_helper.rb
+++ app/helpers/doctor_helper.rb
@@ -1,5 +1,5 @@
 module DoctorHelper
   def list_of_doctors_with_speciality(doctors, speciality)
-    if speciality.present? ? doctors.where(speciality: speciality) : doctors
+    speciality.present? ? doctors.where(speciality: speciality) : doctors
   end
 end
