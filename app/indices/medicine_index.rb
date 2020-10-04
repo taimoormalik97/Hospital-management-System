8f062d0a732ed9c7696e88b82bf5c9a1c3ed5443
SatanSoo12 <zarmeen651@gmail.com>
2020-10-05 02:07:51 +0500

    resolve comment

--- app/indices/medicine_index.rb
+++ app/indices/medicine_index.rb
@@ -1,4 +1,4 @@
 ThinkingSphinx::Index.define :medicine, with: :active_record do
   indexes name
-  has hospital_id,  :type => :integer
+  has hospital_id,  type: :integer
 end
--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -1,4 +1,4 @@
 ThinkingSphinx::Index.define :user, with: :active_record do
   indexes name
-  has hospital_id,  :type => :integer
+  has hospital_id,  type: :integer
 end
